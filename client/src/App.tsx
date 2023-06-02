4d83f7f2893fd4b417d617e9f511c4585cb78a4f
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-06-02 18:06:11 +0200

    install axios and fetch the initial data from the /campgrounds api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,41 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
+type Campground_Type = {
+  title: string;
+  description: string;
+  price: number;
+  location: string;
+  imgUrl?: string;
+};
+
 export default function App() {
+  const [campgroundsData, setCampgroundsData] = useState<Campground_Type[]>([]);
+
+  useEffect(() => {
+    const fetchCampgrounds = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/campgrounds');
+        const data = await response.data;
+        setCampgroundsData(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchCampgrounds();
+  }, []);
+
   return (
     <div>
-      <h1>Jelp Kemp</h1>
+      <ul>
+        {campgroundsData.map((camp, id) => (
+          <li key={id}>
+            {camp.title} ({camp.location}) - ${camp.price}/night
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
