c87f6de2797fc84a16dbe55477b772381ee794a4
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-06-12 12:13:47 +0200

    [client side] add review form into the details page

--- client/src/components/campground/Details.tsx
+++ client/src/components/campground/Details.tsx
@@ -56,33 +56,76 @@ export default function Details() {
         <NotFound status={notFound.status} message={notFound.message} />
       ) : (
         <div className={`container my-4 ${width < 576 && 'px-4'}`}>
-          <div className={`card ${width >= 992 ? 'w-50' : 'w-75'} mx-auto`}>
-            <img
-              src={`${campground?.image}`}
-              className='card-img-top'
-              alt={campground?.title}
-            />
-            <div className='card-body'>
-              <h5 className='card-title'>{campground?.title} </h5>
+          <div className={`${width >= 992 ? 'w-50' : 'w-75'} mx-auto`}>
+            <div className={`card w-auto`}>
+              <img
+                src={`${campground?.image}`}
+                className='card-img-top'
+                alt={campground?.title}
+              />
+              <div className='card-body'>
+                <h5 className='card-title'>{campground?.title} </h5>
 
-              <p className='card-text'>{campground?.description}</p>
+                <p className='card-text'>{campground?.description}</p>
+              </div>
+              <ul className='list-group list-group-flush'>
+                <li className='list-group-item'>{campground?.location}</li>
+                <li className='list-group-item'>${campground?.price}/night</li>
+              </ul>
+              <div className='card-body'>
+                <Link
+                  to={`/campgrounds/${campground?._id}/edit`}
+                  className='btn btn-primary me-2'
+                >
+                  Edit
+                </Link>
+                <button className='btn btn-danger' onClick={handleDelete}>
+                  Delete
+                </button>
+              </div>
+              <div className='card-footer text-body-secondary'>2 days ago</div>
             </div>
-            <ul className='list-group list-group-flush'>
-              <li className='list-group-item'>{campground?.location}</li>
-              <li className='list-group-item'>${campground?.price}/night</li>
-            </ul>
-            <div className='card-body'>
-              <Link
-                to={`/campgrounds/${campground?._id}/edit`}
-                className='btn btn-primary me-2'
+            <div className={`w-auto`}>
+              <h2 className='pt-3 pb-2 m-0'>Leave a review</h2>
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  console.log(e.target);
+                }}
               >
-                Edit
-              </Link>
-              <button className='btn btn-danger' onClick={handleDelete}>
-                Delete
-              </button>
+                <div className='mb-3'>
+                  <label htmlFor='rating' className='form-label fw-medium'>
+                    Rating
+                  </label>
+                  <input
+                    type='range'
+                    className='form-range'
+                    name='rating'
+                    min={1}
+                    max={5}
+                    id='rating'
+                  />
+                </div>
+
+                <div className='mb-3'>
+                  <label htmlFor='text' className='form-label fw-medium'>
+                    Review
+                  </label>
+                  <textarea
+                    className={`form-control`}
+                    id='text'
+                    name='text'
+                    rows={4}
+                  />
+                </div>
+                <button
+                  type='submit'
+                  className='btn btn-success py-2 fw-medium'
+                >
+                  Submit
+                </button>
+              </form>
             </div>
-            <div className='card-footer text-body-secondary'>2 days ago</div>
           </div>
         </div>
       )}
