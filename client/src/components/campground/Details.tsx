742a8b430b8a3b1e1f1768b428fb81f69e57b288
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-06-14 19:21:52 +0200

    [client side] navigate and toast when a campground is not found

--- client/src/components/campground/Details.tsx
+++ client/src/components/campground/Details.tsx
@@ -11,8 +11,6 @@ import axios from 'axios';
 import { useFormik } from 'formik';
 
 import { Campground_Type } from '../../shared/types';
-import { NotFound_Type } from './types';
-import { NotFound } from './ErrorTemplate';
 import { ReviewSchema } from '../../shared/schemas';
 import ReviewForm from './ReviewForm';
 import CustomSnackbar from '../CustomSnackbar';
@@ -21,10 +19,6 @@ export default function Details() {
   const { _id } = useParams();
   const [campground, setCampground] = useState<Campground_Type>();
   const [isValidReview, setIsValidReview] = useState<boolean>(false);
-  const [notFound, setNotFound] = useState<NotFound_Type>({
-    status: 0,
-    message: '',
-  });
   const navigate: NavigateFunction = useNavigate();
   const location: Location = useLocation();
 
@@ -37,9 +31,8 @@ export default function Details() {
         const campgroundFromDb = await response.data;
         setCampground(campgroundFromDb);
       } catch (err: any) {
-        setNotFound({
-          status: err.response.status,
-          message: err.response.data,
+        navigate('/campgrounds', {
+          state: { status: 'error', message: 'Cannot find that campground!' },
         });
       }
     };
@@ -107,62 +100,58 @@ export default function Details() {
 
   return (
     <>
-      {notFound.status === 404 ? (
-        <NotFound status={notFound.status} message={notFound.message} />
-      ) : (
-        <div className='row'>
-          <CustomSnackbar location={location} />
-          <div className='col-6'>
-            <div className='card mb-3'>
-              <img
-                src={`${campground?.image}`}
-                className='card-img-top'
-                alt={campground?.title}
-              />
-              <div className='card-body'>
-                <h5 className='card-title'>{campground?.title} </h5>
+      <div className='row'>
+        <CustomSnackbar location={location} />
+        <div className='col-6'>
+          <div className='card mb-3'>
+            <img
+              src={`${campground?.image}`}
+              className='card-img-top'
+              alt={campground?.title}
+            />
+            <div className='card-body'>
+              <h5 className='card-title'>{campground?.title} </h5>
 
-                <p className='card-text'>{campground?.description}</p>
-              </div>
-              <ul className='list-group list-group-flush'>
-                <li className='list-group-item'>{campground?.location}</li>
-                <li className='list-group-item'>${campground?.price}/night</li>
-              </ul>
-              <div className='card-body'>
-                <Link
-                  to={`/campgrounds/${campground?._id}/edit`}
-                  className='btn btn-primary me-2'
-                >
-                  Edit
-                </Link>
-                <button className='btn btn-danger' onClick={handleDelete}>
-                  Delete
-                </button>
-              </div>
-              <div className='card-footer text-body-secondary'>2 days ago</div>
+              <p className='card-text'>{campground?.description}</p>
             </div>
-          </div>
-          <div className='col-6'>
-            <ReviewForm {...formik} isValidReview={isValidReview} />
-            <div className='card'>
-              <ul className='list-group list-group-flush'>
-                {campground?.reviews.map((review) => (
-                  <li className='list-group-item' key={review._id}>
-                    <h5 className='card-title'>Rating: {review.rating}</h5>
-                    <p className='card-text'>Review: {review.text}</p>
-                    <button
-                      className='btn btn-sm btn-danger'
-                      onClick={() => handleReviewDelete(review._id)}
-                    >
-                      Delete
-                    </button>
-                  </li>
-                ))}
-              </ul>
+            <ul className='list-group list-group-flush'>
+              <li className='list-group-item'>{campground?.location}</li>
+              <li className='list-group-item'>${campground?.price}/night</li>
+            </ul>
+            <div className='card-body'>
+              <Link
+                to={`/campgrounds/${campground?._id}/edit`}
+                className='btn btn-primary me-2'
+              >
+                Edit
+              </Link>
+              <button className='btn btn-danger' onClick={handleDelete}>
+                Delete
+              </button>
             </div>
+            <div className='card-footer text-body-secondary'>2 days ago</div>
           </div>
         </div>
-      )}
+        <div className='col-6'>
+          <ReviewForm {...formik} isValidReview={isValidReview} />
+          <div className='card'>
+            <ul className='list-group list-group-flush'>
+              {campground?.reviews.map((review) => (
+                <li className='list-group-item' key={review._id}>
+                  <h5 className='card-title'>Rating: {review.rating}</h5>
+                  <p className='card-text'>Review: {review.text}</p>
+                  <button
+                    className='btn btn-sm btn-danger'
+                    onClick={() => handleReviewDelete(review._id)}
+                  >
+                    Delete
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
